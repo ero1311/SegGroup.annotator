d0aa6fd4565c98efb8dbe9068a2afc896d49fb3c
Erik <erik@superannotate.com>
2022-05-02 00:17:01 +0400

    remove annotation with gt part

--- annotator_with_gt/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-
-import RouterBoard from "./RouterBoard";
-
-ReactDOM.render(<RouterBoard />, document.getElementById("root"));
